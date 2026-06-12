ee990fd697ae7f360326696fcf8e811d16baffd7
lanzac <a.lanrezac@gmail.com>
2026-06-12 10:14:24 +0200

    feat: add script to get species common name

---
+++ scripts/tools/get_common_name.py
@@ -0,0 +1,40 @@
+import json
+import urllib.parse
+import urllib.request
+
+species_acronyms = [
+    ("Anopheles gambiae", "Agam"),
+    ("Apolygus lucorum", "Aluc"),
+    ("Bactrocera minax", "Bmax"),
+    ("Bombyx mori", "Bmor"),
+    ("Dendroctonus ponderosae", "Dpon"),
+    ("Drosophila melanogaster", "Dmel"),
+    ("Harpegnathos saltator", "Hsal"),
+    ("Helicoverpa armigera", "Harm"),
+    ("Helicoverpa assulta", "Hass"),
+    ("Hylobius abietis", "Habi"),
+    ("Ips typographus", "Ityp"),
+    ("Lampronia capitella", "Lcap"),
+    ("Locusta migratoria", "Lmig"),
+    ("Plutella xylostella", "Pxyl"),
+    ("Spodoptera frugiperda", "Sfru"),
+    ("Spodoptera littoralis", "Slit"),
+]
+
+print("species name,acronym,common_name,ncbi_taxon_id")
+
+for species, acronym in species_acronyms:
+    url = f"https://rest.uniprot.org/taxonomy/search?query={urllib.parse.quote(species)}&fields=id,scientific_name,common_name&format=json&size=1"
+
+    req = urllib.request.Request(url, headers={"Accept": "application/json"})
+    with urllib.request.urlopen(req) as r:
+        data = json.loads(r.read())
+
+    hits = data.get("results", [])
+    if hits:
+        taxon_id = hits[0].get("taxonId", "")
+        common_name = hits[0].get("commonName", "")
+    else:
+        taxon_id, common_name = "", ""
+
+    print(f"{species},{acronym},{common_name},{taxon_id}")
